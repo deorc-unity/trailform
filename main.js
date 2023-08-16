182353bee707d090fd5acb61591db83be53800c2
deorc-unity <95616236+deorc-unity@users.noreply.github.com>
2023-08-16 13:47:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+let sheetID = '1VMyYVbpkgPFRFhXAiNL7J3fUbICI1_-BuyTVXRx9LME'
+let sheetTitle = 'lol';
+
+let sheetRange = 'B4:C10';
+
+let fullURL = ('https://docs.google.com/spreadsheets/d/' + sheetID + '/gviz/tq?sheet=' + sheetTitle + '&range=' + sheetRange);
+
